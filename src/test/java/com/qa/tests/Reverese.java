b5f650a52ef602264a0494ce4f14b6928d8fa118
soumyajahagirdar <soumyajhbl@gmail.com>
2024-06-18 18:13:38 +0530

    first commit..

---
+++ src/test/java/com/qa/tests/Reverese.java
@@ -0,0 +1,19 @@
+package com.qa.tests;
+
+public class Reverese {
+	
+	public static void Reverse() {
+		
+		String s="soumya";
+		
+		char ch[]=s.toCharArray();
+		
+		for(int i=ch.length-1;i>=0;i--) {
+			
+			System.out.println(ch);
+		}
+		
+		
+	}
+
+}
